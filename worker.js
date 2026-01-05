f0c082e2baa06d45746c72e2fc64796149b7fc68
alihasnainh3techs <alimurtaza32721@gmail.com>
2026-01-05 14:34:22 +0500

    worker created.

---
+++ worker.js
@@ -0,0 +1,18 @@
+import { Worker } from "bullmq";
+import redis from "./lib/redis.js";
+
+const worker = new Worker("inventory-alerts-queue", async => {
+
+    // call DB to get shop and product details
+    // send email 
+    // call db for logs
+
+}, { connection: redis, concurrency: 20 });
+
+worker.on('completed', job => {
+    console.log(`${job.id} has completed!`);
+});
+
+worker.on('failed', (job, err) => {
+    console.log(`${job.id} has failed with ${err.message}`);
+});
