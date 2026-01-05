b8d76df6ffd9c729db611782a3fe5d523b9667cf
alihassnain-github <alimurtaza32721@gmail.com>
2026-01-05 23:07:42 +0500

    shopify app SDK installed.

--- worker.js
+++ worker.js
@@ -1,18 +1,20 @@
 import { Worker } from "bullmq";
 import redis from "./lib/redis.js";
 
-const worker = new Worker("inventory-alerts-queue", async => {
+const worker = new Worker(
+    "inventory-alerts-queue",
+    (async) => {
+        // call DB to get shop and product details
+        // send email
+        // call db for logs
+    },
+    { connection: redis, concurrency: 20 }
+);
 
-    // call DB to get shop and product details
-    // send email 
-    // call db for logs
-
-}, { connection: redis, concurrency: 20 });
-
-worker.on('completed', job => {
+worker.on("completed", (job) => {
     console.log(`${job.id} has completed!`);
 });
 
-worker.on('failed', (job, err) => {
+worker.on("failed", (job, err) => {
     console.log(`${job.id} has failed with ${err.message}`);
 });
